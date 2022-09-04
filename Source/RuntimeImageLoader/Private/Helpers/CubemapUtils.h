39dd7f0e18ade6d367f0f6c5314554b9eed75512
Petr Leontev <raianknight@gmail.com>
2022-09-04 10:24:11 +0100

    Fix compilation errors

--- Source/RuntimeImageLoader/Private/Helpers/CubemapUtils.h
+++ Source/RuntimeImageLoader/Private/Helpers/CubemapUtils.h
@@ -1,9 +1,10 @@
 ﻿// Copyright 2022 Peter Leontev. All Rights Reserved.
 
 #include "CoreMinimal.h"
+#include "ImageCore.h"
 
 
 namespace RuntimeCubemapUtils
 {
-    static void GenerateBaseCubeMipFromLongitudeLatitude2D(struct FImage* OutMip, const FImage& SrcImage, const uint32 MaxCubemapTextureResolution, uint8 SourceEncodingOverride);
+    static void GenerateBaseCubeMipFromLongitudeLatitude2D(FImage* OutMip, const FImage& SrcImage, const uint32 MaxCubemapTextureResolution, uint8 SourceEncodingOverride);
 };
