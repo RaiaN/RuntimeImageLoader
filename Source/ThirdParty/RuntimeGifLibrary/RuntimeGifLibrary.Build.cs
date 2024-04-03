3240df30a85567a33edd38bd141ce697a0d39752
Petr Leontev <raianknight@gmail.com>
2024-04-03 19:41:18 +0100

    Fix linking

--- Source/ThirdParty/RuntimeGifLibrary/RuntimeGifLibrary.Build.cs
+++ Source/ThirdParty/RuntimeGifLibrary/RuntimeGifLibrary.Build.cs
@@ -31,16 +31,11 @@ public class RuntimeGifLibrary : ModuleRules
             PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "Linux", "libsharpyuv.a"));
             PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "Linux", "libwebpdecoder.a"));
             PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "Linux", "libwebpdemux.a"));
-
-            PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "Linux", "libwebp.la"));
-            PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "Linux", "libsharpyuv.la"));
-            PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "Linux", "libwebpdecoder.la"));
-            PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "Linux", "libwebpdemux.la"));
         }
         else if (Target.Platform == UnrealTargetPlatform.Mac)
         {
             PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "Mac", "libnsgif.a"));
-            // TODO:
+            // TODO: Add libwebp
         }
         else if (Target.Platform == UnrealTargetPlatform.Android)
 		{
@@ -50,11 +45,6 @@ public class RuntimeGifLibrary : ModuleRules
             PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "Android", "libsharpyuv.a"));
             PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "Android", "libwebpdecoder.a"));
             PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "Android", "libwebpdemux.a"));
-
-            PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "Android", "libwebp.la"));
-            PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "Android", "libsharpyuv.la"));
-            PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "Android", "libwebpdecoder.la"));
-            PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "Android", "libwebpdemux.la"));
         }
     }
 }
